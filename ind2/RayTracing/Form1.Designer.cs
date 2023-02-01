a6ca6ef227d871e3f211a4c35b296dfbfef2895b
Pekich <93043835+Pekich@users.noreply.github.com>
2023-02-02 00:43:50 +0300

    Add files via upload

---
+++ ind2/RayTracing/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace RayTracing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cubeSpecularCB = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.sphereSpecularCB = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.refractSphereCB = new System.Windows.Forms.CheckBox();
+            this.refractCubeCB = new System.Windows.Forms.CheckBox();
+            this.downWallSpecularCB = new System.Windows.Forms.CheckBox();
+            this.upWallSpecularCB = new System.Windows.Forms.CheckBox();
+            this.rightWallSpecularCB = new System.Windows.Forms.CheckBox();
+            this.leftWallSpecularCB = new System.Windows.Forms.CheckBox();
+            this.backWallSpecularCB = new System.Windows.Forms.CheckBox();
+            this.frontWallSpecularCB = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.Location = new System.Drawing.Point(9, 10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(650, 564);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(664, 181);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ray Trace";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cubeSpecularCB
+            // 
+            this.cubeSpecularCB.AutoSize = true;
+            this.cubeSpecularCB.Location = new System.Drawing.Point(6, 19);
+            this.cubeSpecularCB.Name = "cubeSpecularCB";
+            this.cubeSpecularCB.Size = new System.Drawing.Size(44, 17);
+            this.cubeSpecularCB.TabIndex = 2;
+            this.cubeSpecularCB.Text = "Куб";
+            this.cubeSpecularCB.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.sphereSpecularCB);
+            this.groupBox1.Controls.Add(this.cubeSpecularCB);
+            this.groupBox1.Location = new System.Drawing.Point(664, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(94, 79);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Зеркальность";
+            // 
+            // sphereSpecularCB
+            // 
+            this.sphereSpecularCB.AutoSize = true;
+            this.sphereSpecularCB.Location = new System.Drawing.Point(6, 42);
+            this.sphereSpecularCB.Name = "sphereSpecularCB";
+            this.sphereSpecularCB.Size = new System.Drawing.Size(47, 17);
+            this.sphereSpecularCB.TabIndex = 2;
+            this.sphereSpecularCB.Text = "Шар";
+            this.sphereSpecularCB.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.refractSphereCB);
+            this.groupBox2.Controls.Add(this.refractCubeCB);
+            this.groupBox2.Location = new System.Drawing.Point(664, 97);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(94, 79);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Прозрачность";
+            // 
+            // refractSphereCB
+            // 
+            this.refractSphereCB.AutoSize = true;
+            this.refractSphereCB.Location = new System.Drawing.Point(6, 42);
+            this.refractSphereCB.Name = "refractSphereCB";
+            this.refractSphereCB.Size = new System.Drawing.Size(47, 17);
+            this.refractSphereCB.TabIndex = 2;
+            this.refractSphereCB.Text = "Шар";
+            this.refractSphereCB.UseVisualStyleBackColor = true;
+            // 
+            // refractCubeCB
+            // 
+            this.refractCubeCB.AutoSize = true;
+            this.refractCubeCB.Location = new System.Drawing.Point(6, 19);
+            this.refractCubeCB.Name = "refractCubeCB";
+            this.refractCubeCB.Size = new System.Drawing.Size(44, 17);
+            this.refractCubeCB.TabIndex = 2;
+            this.refractCubeCB.Text = "Куб";
+            this.refractCubeCB.UseVisualStyleBackColor = true;
+         
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(770, 585);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Ray Tracing";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox cubeSpecularCB;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox sphereSpecularCB;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox refractSphereCB;
+        private System.Windows.Forms.CheckBox refractCubeCB;
+        private System.Windows.Forms.CheckBox frontWallSpecularCB;
+        private System.Windows.Forms.CheckBox rightWallSpecularCB;
+        private System.Windows.Forms.CheckBox leftWallSpecularCB;
+        private System.Windows.Forms.CheckBox backWallSpecularCB;
+        private System.Windows.Forms.CheckBox downWallSpecularCB;
+        private System.Windows.Forms.CheckBox upWallSpecularCB;
+    }
+}
+
